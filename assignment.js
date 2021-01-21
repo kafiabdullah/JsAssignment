010f9fd30afc5d131f6258937a0439eb501ffd64
kafi <abdullahkafi996@gmail.com>
2021-01-22 02:02:33 +0600

    complete kilometerToMeter function calculation

--- assignment.js
+++ assignment.js
@@ -1,1 +1,18 @@
 //  https://github.com/kafiabdullah/JsAssignment
+
+/**
+ * kilometer to meter calculation
+ */
+function kilometerToMeter(kilometer){
+    // check input validation
+    if(kilometer < 0){
+        return "Kilometer can not ber 0 and negetive"
+    }
+    else if(typeof kilometer == 'string'){
+        return "Kilometer can not be string"
+    }
+
+    // kilometer to meter calculation
+    var resultOfKilometerToMeter = kilometer * 1000;
+    return resultOfKilometerToMeter;
+}
